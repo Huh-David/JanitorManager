a797402cddd8e39cec7cdb1633df2e2c80677926
David Huh <github@davidhuh.de>
2022-02-13 20:57:20 +0100

    :sparkles: Added scrollbar
    - not the right position, but it's getting there

--- src/main/kotlin/de/davidhuh/janitormanager/ui/screens/AllTodosScreen.kt
+++ src/main/kotlin/de/davidhuh/janitormanager/ui/screens/AllTodosScreen.kt
@@ -1,6 +1,10 @@
 package de.davidhuh.janitormanager.ui.screens
 
+import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.OutlinedButton
@@ -14,6 +18,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import de.davidhuh.janitormanager.domain.Todo
 import de.davidhuh.janitormanager.ui.navcontroller.NavController
+import androidx.compose.foundation.HorizontalScrollbar
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.WindowState
+import androidx.compose.ui.window.singleWindowApplication
 
 @Composable
 fun todoOverviewRow(
@@ -52,13 +77,28 @@ fun todoOverviewRow(
 fun allTodosScreen(
 	navController: NavController,
 ) {
-	Column(modifier = Modifier.padding(start = 80.dp)) {
+	val stateVertical = rememberScrollState(0)
+
+
+	VerticalScrollbar(
+		modifier = Modifier
+			.fillMaxHeight(),
+		adapter = rememberScrollbarAdapter(stateVertical)
+	)
+
+	Column(
+		modifier = Modifier
+			.padding(start = 80.dp)
+			.verticalScroll(stateVertical)
+	) {
+
+
 		navController.cleaningObjectList.forEachIndexed { index, cleaningObject ->
-			cleaningObject.activityList.forEach { activity ->
+			for (activity in cleaningObject.activityList) {
 				val todo: Todo = if (activity.todoList.none() { !it.done }) {
-					remember { activity.todoList.last() }
+					continue
 				} else {
-					remember { activity.todoList.first() { !it.done } }
+					activity.todoList.first() { !it.done }
 				}
 				val text = "$cleaningObject ${activity.activityType}"
 				val todoText: MutableState<String> = remember { mutableStateOf("$todo") }
@@ -72,7 +112,13 @@ fun allTodosScreen(
 					},
 					onClick2 = {
 						todo.changeStatus()
-						todoText.value = "$todo"
+
+						if (activity.todoList.none() { !it.done }) {
+							todoText.value = "Everything Done"
+						} else {
+							todoText.value = "${activity.todoList.first() { !it.done }}"
+						}
+
 					},
 				)
 			}

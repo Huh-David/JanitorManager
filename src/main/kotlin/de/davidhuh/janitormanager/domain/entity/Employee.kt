bb8f6e4e1082f0680ec586d5ec8fdaaaa0e26074
David Huh <github@davidhuh.de>
2022-02-25 15:00:13 +0100

    :bug: Fixed unit tests

--- src/main/kotlin/de/davidhuh/janitormanager/domain/entity/Employee.kt
+++ src/main/kotlin/de/davidhuh/janitormanager/domain/entity/Employee.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 import java.util.UUID
 
 @Serializable
-abstract class Employee {
+sealed class Employee {
 	@Serializable(with = HelperService.UUIDSerializer::class)
 	abstract val employeeId: UUID
 	abstract val preName: String
